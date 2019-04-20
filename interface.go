72f4155de18af359a3e9a925485cf842e50a19e1
maxim-ge <maxim.ge@gmail.com>
2019-04-20 11:26:21 +0300

    Get refactored

--- interface.go
+++ interface.go
@@ -9,18 +9,16 @@ package istorage
 
 import "context"
 
-// PutBatch puts record in transaction
+// PutBatch puts records in transaction
 // WorkspaceID from records are ignored, one from parameters are used
 var PutBatch func(ctx context.Context, workspaceID int64, batch []*Record) error
 
-// Get returns a record, nil if not found
+// GetByID returns a record, nil if not found
 // If partTypes is empty all parts are returned
-var Get func(ctx context.Context, workspaceID int64, recordType int32, ID int64, partTypes []int32) (*Record, error)
+var GetByID func(ctx context.Context, workspaceID int64, recordType int32, ID int64, partTypes []int32) (*Record, error)
 
-// GetAll returns all records of the given type
+// Get returns records with given workspaceID and recordType
+// If useID is true result is filtered by ID
+// If partType is not empty result is filtered by partTypes
 // GetAll must analyze ctx.Done
-// If partType is empty all parts are returned
-var GetAll func(ctx context.Context, wsID int64, recordType int32, partTypes []int32) (chan *Record, error)
-
-// WipeWorkspace removes all records in workspace
-var WipeWorkspace func(ctx context.Context, wsID int64) error
+var Get func(ctx context.Context, workspaceID int64, recordType int32, useID bool, ID int64, partTypes []int32) (chan *Record, error)
